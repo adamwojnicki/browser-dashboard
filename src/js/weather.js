fe7e87094ca0f462e41429d75cd6c6c77dba25b8
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-01-26 21:28:16 +0100

    Working on weather API

---
+++ src/js/weather.js
@@ -0,0 +1,9 @@
+import "dotenv";
+
+export const weather = () => {
+  const apiKey = "";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw,PL&appid=${apiKey}`;
+  fetch(url)
+    .then(resp => resp.json())
+    .then(data => console.log(data));
+};
